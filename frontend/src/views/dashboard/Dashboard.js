84fd083ed539df0ff7c0169cefbfd1d620e024a4
Yasmine Ben Slim <yasmine.benslim@medtech.tn>
2024-06-11 15:39:07 +0100

    update dashboard.js
    image + name + XP + role

--- frontend/src/views/dashboard/Dashboard.js
+++ frontend/src/views/dashboard/Dashboard.js
@@ -3,14 +3,10 @@ import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 import {
   CAvatar,
-  CButton,
-  CButtonGroup,
   CCard,
   CCardBody,
-  CCardFooter,
   CCardHeader,
   CCol,
-  CProgress,
   CRow,
   CTable,
   CTableBody,
@@ -21,12 +17,9 @@ import {
 } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 import { cilPeople } from '@coreui/icons';
-import WidgetsBrand from '../widgets/WidgetsBrand';
 import WidgetsDropdown from '../widgets/WidgetsDropdown';
-import MainChart from './MainChart';
 import userAvatar from '../../components/Images/user.png';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
+
 import './Dashboard.css'; 
 import TaskCalendar from './TaskCalendar'
 
@@ -51,10 +44,8 @@ const Dashboard = () => {
                     <CTableHeaderCell className="bg-body-tertiary text-center">
                       <CIcon icon={cilPeople} />
                     </CTableHeaderCell>
-                    <CTableHeaderCell className="bg-body-tertiary">User</CTableHeaderCell>
-                    <CTableHeaderCell className="bg-body-tertiary text-center">Birthday</CTableHeaderCell>
-                    <CTableHeaderCell className="bg-body-tertiary">Email</CTableHeaderCell>
-                    <CTableHeaderCell className="bg-body-tertiary text-center">Phone</CTableHeaderCell>
+                    <CTableHeaderCell className="bg-body-tertiary">Name</CTableHeaderCell>
+                    <CTableHeaderCell className="bg-body-tertiary text-center">Points</CTableHeaderCell>
                     <CTableHeaderCell className="bg-body-tertiary">Role</CTableHeaderCell>
                   </CTableRow>
                 </CTableHead>
@@ -71,9 +62,9 @@ const Dashboard = () => {
                       <CTableDataCell>
                         <div>{user.username}</div>
                       </CTableDataCell>
-                      <CTableDataCell className="text-center">{user.birthday}</CTableDataCell>
-                      <CTableDataCell>{user.email}</CTableDataCell>
-                      <CTableDataCell className="text-center">{user.phone}</CTableDataCell>
+                      {/* Display updated points */}
+                      <CTableDataCell className="text-center">{user.exp}</CTableDataCell>
+                      {/* End of displaying updated points */}
                       <CTableDataCell>
                         <div className="fw-semibold text-nowrap">{user.role}</div>
                       </CTableDataCell>
@@ -95,6 +86,7 @@ const Dashboard = () => {
       </CRow>
 
     </>
+
   );
 };
 
